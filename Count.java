e3a7eb41a4d4d393de3bca5dca5dbfa4140b458e
varshitha55 <41734732+varshitha55@users.noreply.github.com>
2018-08-01 13:54:18 +0530

    Update Count.java

--- Count.java
+++ Count.java
@@ -10,12 +10,12 @@ class Count
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc=new Scanner(System.in);
+			Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		int count=0;
+		int count=1;
 		for(int i=0;i<str.length();i++)
 		{
-			if(str.charAt(i)==' ')
+			if(str.charAt(i)==' ' && str.charAt(i+1)!=' ')
 			count++;
 		}
 		System.out.println(count);
